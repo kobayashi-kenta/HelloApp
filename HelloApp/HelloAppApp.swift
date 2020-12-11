841b88255055e42d7cfa8d7abcebe09c0785f732
kobayashi-kenta <12321klj@gmail.com>
2020-12-11 23:33:51 +0900

    Initial Commit

---
+++ HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by Kenta on 2020/12/11.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
